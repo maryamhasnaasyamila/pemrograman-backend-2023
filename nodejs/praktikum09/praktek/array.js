4b27a1d343e13884587107fd48fa486afef33a1f
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2023-12-01 10:39:15 +0700

    praktikum 9

---
+++ nodejs/praktikum09/praktek/array.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"]
+
+console.log(fruits[0], fruits[1]);
+
+for(const fruit of fruits) {
+    console.log(fruits);
+}
---
+++ nodejs/praktikum09/praktek/object.js
@@ -0,0 +1,9 @@
+const user = {
+  name: "Hasna",
+  address: "Depok",
+  age: 18,
+  isMarried: false,
+};
+
+// mengakses properti objek
+console.log(user.name);
