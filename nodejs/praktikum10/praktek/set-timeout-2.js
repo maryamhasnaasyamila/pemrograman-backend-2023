fff75245b8032a2bccfe67fbfe33464824186603
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2023-12-08 09:46:13 +0700

    praktikum 10 backend

---
+++ nodejs/praktikum10/praktek/set-timeout-2.js
@@ -0,0 +1,8 @@
+console.log("Hasna membuka browser Chrome");
+
+setTimeout(() => {
+  console.log("Mendownload file 1 jam");
+  console.log("Download selesai");
+}, 5000);
+
+console.log("Hasna membuka YouTube");
---
+++ nodejs/praktikum10/praktek/set-timeout.js
@@ -0,0 +1,6 @@
+console.log("Satu");
+setTimeout(function(){
+    console.log("Dua");
+},300);
+
+console.log("Tiga");
